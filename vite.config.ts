4100a70dd5c288ff1551a7c5a0e04c05635235c7
JulioC090 <juliocziviani@gmail.com>
2026-03-16 23:01:42 -0300

    chore: set base path for Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,6 @@ import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
+  base: "/test-md-stream/",
   plugins: [tailwindcss(), react()],
 });
